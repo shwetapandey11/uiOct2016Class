cef9ac65aa4c2ea42b53146acec1a625fc9dcd31
shwetapandey <shwetapandey11@gmail.com>
2016-12-06 00:31:06 -0800

    closure hw

--- javascript/12_05_2016-Closures/shweta/closures.js
+++ javascript/12_05_2016-Closures/shweta/closures.js
@@ -1,1 +1,34 @@
-alert("hello");
+// IIFE Construct
+(function(){
+	function total(){
+		var myVar = this;
+
+		function mathematics(a,b){			
+			var myVar2 = this;
+			myVar2.sum = function(){
+				return (a + b);
+			}
+			myVar2.difference = function(){
+				return  (a - b);
+			}
+			myVar2.multiplication = function(){
+				return (a * b);
+			}
+			myVar2.division = function(){
+				return (a / b);
+			}
+		}
+
+		var m = new mathematics(5,3);
+		myVar.addition = m.sum();
+		myVar.substract = m.difference();
+		myVar.product = m.multiplication();
+		myVar.dividend = m.division();
+
+		console.log (myVar.addition);
+		console.log (myVar.substract);
+		console.log (myVar.product);
+		console.log (myVar.dividend);	
+	}
+	var answer2 = new total();
+} )();
